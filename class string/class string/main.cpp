9d0f61fae86f7995383a1b90f3dece92eac6e809
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2024-01-08 23:02:49 +0800

    Add files via upload

---
+++ class string/class string/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  class string
+//
+//  Created by Nisha Ramprasath on 2/11/23.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    string str;
+    cout<<"enter a string";
+    getline(cin,str);
+    
+    cout<<str<<endl;
+    return 0;
+}
